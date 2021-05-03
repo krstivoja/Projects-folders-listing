eb408d9e8f90e127510597b578fd40708c1f3080
marko <mailme@markokrstic.com>
2021-05-03 15:08:46 +0200

    update

--- Folder listing with login/index.php
+++ Folder listing with login/index.php
@@ -1,24 +1,19 @@
 <?php
     session_start();
-
-        if(isset($_POST['submit_pass']) && $_POST['pass'])
-            {
-                $pass=$_POST['pass'];
-                    if($pass=="dev")
-                        {
-                            $_SESSION['password']=$pass;
-                         }
-                    else
-                        {
-                            $error="Incorrect Password";
-                    }
-                }
-
-        if(isset($_POST['page_logout']))
-            
-            {
-                unset($_SESSION['password']);
+    $error = '';
+    if(isset($_POST['submit_pass']) && $_POST['pass']){
+        $pass = $_POST['pass'];
+        if($pass == "dev"){
+            $_SESSION['password'] = $pass;
         }
+        else {
+            $error = "Incorrect Password";
+        }
+    }
+
+    if(isset($_POST['page_logout'])){
+        unset($_SESSION['password']);
+    }
 ?>
 
 <html>
@@ -29,27 +24,30 @@
 
 <body>
 <?php   
-    if($_SESSION['password']=="dev") {   
+    $thelist = "";
+    if(isset($_SESSION['password']) && $_SESSION['password']=="dev") {   
  
         if ($handle = opendir('.')) {
+            $ignoreList = array('cgi-bin', '.', '..', '._');
             while (false !== ($file = readdir($handle))) {
-                if (is_dir($file)) {
-                    if ($file=='.'||$file=='..') continue;
+              if (is_dir($file) && (!in_array($file, $ignoreList) and substr($file, 0, 1) != '.')) {
+                    if ($file == '.'|| $file == '..') continue;
 
                         $thelist .= '<li>
-                                        <span>
-                                            <span class="icon folder">
-                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M464 128H272l-64-64H48C21.49 64 0 85.49 0 112v288c0 26.51 21.49 48 48 48h416c26.51 0 48-21.49 48-48V176c0-26.51-21.49-48-48-48z"/></svg>
-                                                </span>
-                                                <span class="icon folder-open">
-                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M572.694 292.093L500.27 416.248A63.997 63.997 0 0 1 444.989 448H45.025c-18.523 0-30.064-20.093-20.731-36.093l72.424-124.155A64 64 0 0 1 152 256h399.964c18.523 0 30.064 20.093 20.73 36.093zM152 224h328v-48c0-26.51-21.49-48-48-48H272l-64-64H48C21.49 64 0 85.49 0 112v278.046l69.077-118.418C86.214 242.25 117.989 224 152 224z"/></svg>
-                                            </span>
-                                            
-                    				    <a target="_blank" class="dir" href="'.$file.'">'.$file.'</a>
-                                        </span>
-
-                    				    <a target="_blank" class="admin" href="'.$file.'/wp-admin/">Login</a>
-                    			     </li>';
+                            <span>
+                                <span class="icon folder">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M464 128H272l-64-64H48C21.49 64 0 85.49 0 112v288c0 26.51 21.49 48 48 48h416c26.51 0 48-21.49 48-48V176c0-26.51-21.49-48-48-48z"/></svg>
+                                    </span>
+                                    <span class="icon folder-open">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M572.694 292.093L500.27 416.248A63.997 63.997 0 0 1 444.989 448H45.025c-18.523 0-30.064-20.093-20.731-36.093l72.424-124.155A64 64 0 0 1 152 256h399.964c18.523 0 30.064 20.093 20.73 36.093zM152 224h328v-48c0-26.51-21.49-48-48-48H272l-64-64H48C21.49 64 0 85.49 0 112v278.046l69.077-118.418C86.214 242.25 117.989 224 152 224z"/></svg>
+                                </span>
+                                
+        				    <a target="_blank" class="dir" href="'.$file.'">'.$file.'</a>
+                            </span>';
+                            if(file_exists($file.'/wp-load.php')){
+                              $thelist .= '<a target="_blank" class="admin" href="'.$file.'/wp-admin/">Login</a>';
+                            }
+                        $thelist .= '</li>';
                 }
         }
         
